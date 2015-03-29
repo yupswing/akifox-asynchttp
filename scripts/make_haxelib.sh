29862b98ca5741e17f01ea7a93b5a0daac22b316
yupswing <simonecingano@gmail.com>
2015-03-29 18:09:08 +0200

    TAG 0.3.0 + removed samples from haxelib release

--- scripts/make_haxelib.sh
+++ scripts/make_haxelib.sh
@@ -1,3 +1,2 @@
 rm -rf bin/haxelib-akifox-asynchttp.zip &&
-rm -rf samples/simple/bin &&
-zip bin/haxelib-akifox-asynchttp.zip -r samples com README.md LICENSE haxelib.json;
+zip bin/haxelib-akifox-asynchttp.zip -r com README.md LICENSE haxelib.json;
